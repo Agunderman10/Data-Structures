946656cf6d29cab68f8616a29958fb4b0eb64381
Andrew Gunderman <agunderman10@gmail.com>
2019-02-01 15:52:09 -0500

    Added stack class and began docs

---
+++ src/Stack.java
@@ -0,0 +1,62 @@
+/* What is a stack?
+ *     A stack is a one-ended linear data structure which models a real world stack by having two primary operations,
+ *     namely push and pop. We can visualize a stack as literally a stack. Think of a stack of books. A visual example
+ *     is shown below:
+ *     
+ *     Data <- this is the top pointer of the stack
+ *     Data
+ *     Data
+ *     Data
+ *     
+ *     When we remove an item from a stack we call it "pop", adding is called "push". Remember, items in a stack 
+ *     always get added and removed from the TOP of the pile. This is commonly known as LIFO, or Last In, First Out.
+ * Push and Pop Examples
+ *     Let's say we have the stack of items below:
+ *     
+ *     Apple
+ *     Potato
+ *     Cabbage
+ *     Garlic
+ *     
+ *     If we want to remove the apple from the stack we call a pop() function. So we do that and our new stack
+ *     looks like this:
+ *     
+ *     Potato
+ *     Cabbage
+ *     Garlic
+ *     
+ *     If we want to add an onion to the stack then we have to call a push() function. So we call push(onion) and 
+ *     our new stack looks like this:
+ *     
+ *     Onion
+ *     Potato
+ *     Cabbage
+ *     Garlic
+ *     
+ *     Next we want to push celery on the stack, so we call push(celery). The new stack looks like this:
+ *     
+ *     Celery
+ *     Onion
+ *     Potato
+ *     Cabbage
+ *     Garlic
+ *     
+ *     Then we want to remove an item, so we call pop(). The new list looks like this:
+ *     
+ *     Onion
+ *     Potato
+ *     Cabbage
+ *     Garlic
+ *     
+ *     REMEMBER we always operate on the top of the stack. We do not have access to any other item in a stack besides
+ *     the top. It is critical to understand this to understand how a stack works.
+ *     
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class Stack {
+
+}
