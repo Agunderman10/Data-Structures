0201a9fef5074fc7cce257d9eac4acc564752507
Andrew Gunderman <agunderman10@gmail.com>
2019-02-18 16:43:44 -0500

    Added what is a queue section

--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,16 @@
+/*
+ * What is a queue?
+ *     A queue is a linear data structure which models real world queues by having two primary operations,
+ *     namely enqueue and dequeue. Every queue has a front and a back. A visual representation is shown below.
+ *     
+ *     elem elem elem elem elem
+ *     ^queue front        ^queue back
+ *     
+ *     In a queue we insert elements through the back and remove through the front. Adding elements to the back of the
+ *     queue if called enqueueing and removing an element from the front is called dequeuing. Sometimes enqueue may
+ *     also be called adding or offering and dequeuing may be called polling or removing.
+ *     
+ */
 
 public class Queue 
 {
