1f59164d7aa9b88ce9e848008127d02afacf6fe0
Andrew Gunderman <agunderman10@gmail.com>
2019-02-23 09:17:17 -0500

    Added When and Where Union Find is used

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -8,6 +8,20 @@
  *     
  *     Find - given an element the UnionFind will tell you what group that element belongs to. 
  *     Union - merges two groups together 
+ *     
+ * When and Where is Union Find used?
+ * 
+ *     Kruskal's Minimum Spanning Tree Algorithm
+ *     
+ *     Grid Percolation - where there are a bunch of dots on a grid and we are trying to see if there is a path from 
+ *     the bottom of the grid to the top of the grid. Union Find solves this problem efficiently by merging together
+ *     paths.
+ *     
+ *     Network Connectivity
+ *     
+ *     Least Common Ancestor in Trees
+ *     
+ *     Image Processing
  * 
  */
 
