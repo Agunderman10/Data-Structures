1cc50b9e6081ffa7a4e69f54c2258287efe4a2d1
Andrew Gunderman <agunderman10@gmail.com>
2019-02-23 09:10:34 -0500

    Added What is UnionFind

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,5 +1,17 @@
+/*
+ * Union Find - also sometimes called a Disjoint Set
+ * 
+ * What is Union Find?
+ * 
+ *     Union Find is a data structure that keeps track of elements which are split into one or more disjoint sets. It
+ *     has two primary operations: find and union. 
+ *     
+ *     Find - given an element the UnionFind will tell you what group that element belongs to. 
+ *     Union - merges two groups together 
+ * 
+ */
 
 public class UnionFind 
 {
-
+	
 }
